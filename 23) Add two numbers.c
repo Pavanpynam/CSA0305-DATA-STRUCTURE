dbd4f6e4a35103d6e664637d8be0a05106c35d0c
Pavanpynam <113625930+Pavanpynam@users.noreply.github.com>
2023-02-13 08:28:40 +0530

    Create 23) Add two numbers.c
    ADD TWO NUMBERS(OUTPUT)

---
+++ 23) Add two numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+	#include<conio.h>
+	int main()
+	{
+	int a=10;
+	int b;
+	int sum;
+	printf("enter the value of b");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("sum of two numbers are:%d",sum);
+	getch();
+	return 0;
+	}
+
+
+OUTPUT:
+enter the value of b23
+sum of two numbers are:33
+
+...Program finished with exit code 0
+Press ENTER to exit console.
